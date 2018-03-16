bd7482a660addb39e9cb035df59e45dcf1f75ce8
Christophe De Troyer <christophe@call-cc.be>
2018-03-16 19:27:50 +0100

    distinct and filter test

--- test/observables_test.exs
+++ test/observables_test.exs
@@ -64,4 +64,51 @@ defmodule ObservablesTest do
 
     assert 5 == 5
   end
+
+  test "Distinct" do
+    testproc = self()
+
+    xs = [1, 1, 1, 1]
+
+    xs
+    |> Obs.from_enum(100)
+    |> Obs.distinct()
+    |> Obs.map(fn v -> send(testproc, v) end)
+
+    Enum.uniq(xs)
+    |> Enum.map(fn x ->
+      receive do
+        ^x -> :ok
+      end
+
+      receive do
+        ^x -> assert "duplicates" == ""
+      after
+        1000 -> :ok
+      end
+    end)
+
+    assert 5 == 5
+  end
+
+  test "Filter" do
+    testproc = self()
+
+    xs = [1, 2, 3, 1, 2, 3, 3, 2, 1]
+
+    xs
+    |> Obs.from_enum(100)
+    |> Obs.filter(fn x -> x > 2 end)
+    |> Obs.map(fn v -> send(testproc, v) end)
+
+    xs
+    |> Enum.filter(fn x -> x > 2 end)
+    |> Enum.map(fn x ->
+      receive do
+        ^x -> :ok
+      end
+    end)
+
+    assert 5 == 5
+  end
 end
